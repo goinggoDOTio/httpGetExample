fa2d37fadc5932d509ca52b2157681a840985a71
Joao Henrique Machado Silva <joaoh82@gmail.com>
2016-11-18 18:25:14 -0200

    Done first http request

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("http://goinggo.io/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	page, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", page)
+}
